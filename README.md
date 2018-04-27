ea4f0a6a5314e9835631b7c9eb220c717a9dc522
Paweł Cisło <pyxelr@gmail.com>
2018-04-27 13:51:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# M23COM - Image Classification Using Convolutional Neural Networks on CIFAR-10
+
+
+The project was done for Artificial Neural Networks module at Coventry University.
+
+The results of running the models can be viewed directly in the hosted Jupyter Notebook files.
+
+The repository includes:
+ * [Decoder]() - set of functions that are required for decoding and fetching the dataset into the IPython notebook
+ * [cifar10_dataset]() - folder with the CIFAR-10 dataset. The content of this folder can be also downloaded from the [official website](http://www.cs.toronto.edu/~kriz/cifar.html) of the CIFAR-10 dataset
+ * [Python script of simple CNN]() in Jupyter Notebook
+ * [Python script of improved CNNs]() in Jupyter Notebook
+ * Model files of saved models - files with .h5 extension
+ * [Report]() - .pdf document in IEEE format
---
+++ decoder.py
@@ -0,0 +1,246 @@
+# Decoder.py - decoder of CIFAR-10 dataset
+
+# Pickle - for decoding the data
+import pickle
+# NumPy - for array manipulations
+import numpy as np
+# Keras - for making one-hot vectors
+from keras.utils import np_utils
+# Matplolib - for ploting graphs and displaying images
+from matplotlib import pyplot as plt
+
+
+
+# Define Constants
+
+path = "cifar10_dataset/"  # Path to dataset
+
+# Height or width of the images (32 x 32)
+size = 32 
+
+# 3 channels of the image: Red, Green, Blue (RGB)
+channels = 3  
+
+# Number of classes
+num_classes = 10 
+
+# Each file in the dataset contains 10000 images
+image_batch = 10000 
+
+# Number of files for training
+num_files_train = 5  
+
+# Calculate total number of training images
+images_train = image_batch * num_files_train
+
+# Function to decode the files
+def unpickle(file):  
+    
+    # Convert byte stream to object
+    with open(path + file,'rb') as fo:
+        print("Decoding file: %s" % (path+file))
+        dict = pickle.load(fo, encoding='bytes')
+       
+    # Return images and labesl in form of a dictionary
+    return dict
+
+# Convert images to NumPy arrays to make them useful
+def convert_images(raw_images):
+    
+    # Convert raw images to numpy array and normalize it
+    raw = np.array(raw_images, dtype = float) / 255.0
+    
+    # Reshape to 4-dimensions - [image_number, channel, height, width]
+    images = raw.reshape([-1, channels, size, size])
+
+    images = images.transpose([0, 2, 3, 1])
+
+    # 4D array - [image_number, height, width, channel]
+    return images
+
+
+
+# Load file, unpickle it and return images with their labels
+def load_data(file):
+    
+    data = unpickle(file)
+    
+    # Get raw images from the dictionary
+    images_array = data[b'data']
+    
+    # Convert image
+    images = convert_images(images_array)
+    
+    # Convert class number to numpy array
+    labels = np.array(data[b'labels'])
+        
+    # Images and labels in np array form
+    return images, labels
+
+
+
+# Load all test data
+def get_test_data():
+    
+    images, labels = load_data(file = "test_batch")
+    
+    # Images, their labels and 
+    # corresponding one-hot vectors in form of np arrays
+    return images, labels, np_utils.to_categorical(labels,num_classes)
+
+
+
+# Load all training data from 5 files
+def get_train_data():
+    
+    # Pre-allocate arrays
+    images = np.zeros(shape = [images_train, size, size, channels], dtype = float)
+    labels = np.zeros(shape=[images_train],dtype = int)
+    
+    # Starting index of training dataset
+    start = 0
+    
+    # For all 5 files
+    for i in range(num_files_train):
+        
+        # Load images and labels
+        images_batch, labels_batch = load_data(file = "data_batch_" + str(i+1))
+        
+        # Calculate end index for current batch
+        end = start + image_batch
+        
+        # Store data to corresponding arrays
+        images[start:end,:] = images_batch        
+        labels[start:end] = labels_batch
+        
+        # Update starting index of next batch
+        start = end
+    
+    # Images, their labels and 
+    # corresponding one-hot vectors in form of np arrays
+    return images, labels, np_utils.to_categorical(labels,num_classes)
+        
+
+# Load class names
+def get_class_names():
+
+    raw = unpickle("batches.meta")[b'label_names']
+
+    # Convert from binary strings
+    names = [x.decode('utf-8') for x in raw]
+
+    # Class names
+    return names
+
+
+# Plot images
+def plot_images(images, labels_true, class_names, labels_pred=None):
+
+    assert len(images) == len(labels_true)
+
+    # Create a figure with sub-plots
+    fig, axes = plt.subplots(3, 3, figsize = (8,8))
+
+    # Adjust the vertical spacing
+    if labels_pred is None:
+        hspace = 0.2
+    else:
+        hspace = 0.5
+    fig.subplots_adjust(hspace=hspace, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Fix crash when less than 9 images
+        if i < len(images):
+            # Plot the image
+            ax.imshow(images[i], interpolation='spline16')
+            
+            # Name of the true class
+            labels_true_name = class_names[labels_true[i]]
+
+            # Show true and predicted classes
+            if labels_pred is None:
+                xlabel = "True: "+labels_true_name
+            else:
+                # Name of the predicted class
+                labels_pred_name = class_names[labels_pred[i]]
+
+                xlabel = "True: "+labels_true_name+"\nPredicted: "+ labels_pred_name
+
+            # Show the class on the x-axis
+            ax.set_xlabel(xlabel)
+        
+        # Remove ticks from the plot
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Show the plot
+    plt.show()
+    
+    
+# Plot model
+def plot_model(model_details):
+
+    # Create sub-plots
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    
+    # Summarize history for accuracy
+    axs[0].plot(range(1,len(model_details.history['acc'])+1),model_details.history['acc'])
+    axs[0].plot(range(1,len(model_details.history['val_acc'])+1),model_details.history['val_acc'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_details.history['acc'])+1),len(model_details.history['acc'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    
+    # Summarize history for loss
+    axs[1].plot(range(1,len(model_details.history['loss'])+1),model_details.history['loss'])
+    axs[1].plot(range(1,len(model_details.history['val_loss'])+1),model_details.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_details.history['loss'])+1),len(model_details.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    
+    # Show the plot
+    plt.show()
+
+
+# Visualize errors
+def visualize_errors(images_test, labels_test, class_names, labels_pred, correct):
+    
+    incorrect = (correct == False)
+    
+    # Images of the test-set that have been incorrectly classified.
+    images_error = images_test[incorrect]
+    
+    # Get predicted classes for those images
+    labels_error = labels_pred[incorrect]
+
+    # Get true classes for those images
+    labels_true = labels_test[incorrect]
+    
+    
+    # Plot the first 9 images.
+    plot_images(images=images_error[0:9],
+                labels_true=labels_true[0:9],
+                class_names=class_names,
+                labels_pred=labels_error[0:9])
+    
+
+# Predict Classes
+def predict_classes(model, images_test, labels_test):
+    
+    # Predict class of image using model
+    class_pred = model.predict(images_test, batch_size=32)
+
+    # Convert vector to a label
+    labels_pred = np.argmax(class_pred,axis=1)
+
+    # Boolean array that tell if predicted label is the true label
+    correct = (labels_pred == labels_test)
+
+    # Array which tells if the prediction is correct or not
+    # And predicted labels
+    return correct, labels_pred
+
+
